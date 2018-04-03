6532471d874cfeead11de1f53057c0ed08ca8dd9
Jaime Fernandez Sanchez <jfesa@alpha11.aulas.gsyc.urjc.es>
2018-04-03 17:37:49 +0200

    Ejercicio 15.5

--- myproject/urls.py
+++ myproject/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'cms.views.inicio'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(.*)', 'cms.views.cms'),
 )
